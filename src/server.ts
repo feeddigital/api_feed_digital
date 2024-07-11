5c0881507e8ea54bedddec56de7663194c76f195
MauriDiPietro <dipietro.jm@gmail.com>
2024-07-11 01:30:04 -0300

    headers cors

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,16 @@ import config from './config/config';
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', config.ENV === 'prod' ? 'https://front-feed-digital.onrender.com' : 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    console.log('Encabezados CORS configurados');
+    next();
+  });
+
 app.use(cors({
     origin: 'https://front-feed-digital.onrender.com',   
     methods: 'GET,POST,PUT,DELETE,OPTIONS', 
