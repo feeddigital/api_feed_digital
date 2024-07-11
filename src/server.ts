5eed4466d727252a1558545c89229a10b68d6a64
MauriDiPietro <dipietro.jm@gmail.com>
2024-07-11 00:30:43 -0300

    config cors

--- src/server.ts
+++ src/server.ts
@@ -15,8 +15,9 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:5173',   
-    methods: 'GET,POST,PUT,DELETE', 
-    allowedHeaders: 'Content-Type,Authorization' 
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', 
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true 
 }));
 
 const PORT = process.env.PORT || 8080;
