f646a8f00cc913047010880d387139f418c53b5a
MauriDiPietro <dipietro.jm@gmail.com>
2024-07-11 01:04:20 -0300

    origin front

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: '*',   
+    origin: 'https://668f5964d5584dc6dfffa077--eloquent-pony-d86c5d.netlify.app/',   
     methods: 'GET,POST,PUT,DELETE,OPTIONS', 
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true 
